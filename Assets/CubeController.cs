28a8057f809b36cda9aee494c4acfe7071e03750
seiju.hattori <seiju1213@gmail.com>
2018-05-23 23:11:26 +0900

    GoForwardUnityChan
    unitychan２Dゲーム

---
+++ Assets/CubeController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeController : MonoBehaviour {
+
+    private float speed = -0.2f;
+    private float deadLine = -10;
+    public AudioClip sound;
+
+
+    // Use this for initialization
+    void Start () {
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        transform.Translate(this.speed, 0, 0);
+
+        if (transform.position.x < this.deadLine)
+        {
+            Destroy(gameObject);
+        }
+		
+	}
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "block" || other.gameObject.tag == "ground") 
+        {
+            GetComponent<AudioSource>().PlayOneShot(sound);
+        }
+    }
+}
